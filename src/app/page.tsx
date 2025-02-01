97db336eba4f38dfc498188c1e5637f92ffbfc61
Parthan-Akon <parthan.parth123@gmail.com>
2025-02-02 00:55:52 +0530

    Updated page file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="max-w-md p-4 bg-white h-full w-full">
         <h2 className="text-xl font-bold text-gray-800">Expense tracker</h2>
         <p className="text-gray-600">
-          This is first release of Expense tracker application.
+          This is first release of Expense tracker application. Further development will be continued!
         </p>
       </div>
     </div>
