484f47288aa6ceab2b2a8a1e23c3553c77ec161b
Parthan-Akon <parthan.parth123@gmail.com>
2025-02-02 13:35:26 +0530

    Resolved lint errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex justify-center min-h-screen bg-gray-100">
       <div className="max-w-md p-4 bg-white h-full w-full">
         <h2 className="text-xl font-bold text-gray-800">Expense tracker</h2>
         <p className="text-gray-600">
-          This is first release of Expense tracker application. Further development will be continued!
+          This is first release of Expense tracker application. Further
+          development will be continued!
         </p>
       </div>
     </div>
